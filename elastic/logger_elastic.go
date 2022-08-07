d94759f6b5bbeace3bc1c3d75099d88ed13e68f6
guilherme-souza-lima <guilherme_souza.lima@hotmail.com>
2022-08-07 11:00:19 -0300

    :zap::test new struct logger elastic

--- elastic/logger_elastic.go
+++ elastic/logger_elastic.go
@@ -19,6 +19,41 @@ func NewLoggerElastic(Index string, Broker []string) LoggerElastic {
 	return LoggerElastic{Index, Broker}
 }
 
+func (l LoggerElastic) LoggerElasticsearchPatterTest(mapping entity.History) error {
+	log := logger.GetInstance().Logger
+
+	client, err := GetESClient(l.Brokers)
+	if err != nil {
+		log.Error("Error MyLib: " + err.Error())
+		return err
+	}
+
+	dataJSON, err := json.Marshal(mapping)
+	if err != nil {
+		log.Error("Error MyLib: " + err.Error())
+		return err
+	}
+	id := uuid.New()
+
+	req := esapi.IndexRequest{
+		Index:      l.Index,
+		DocumentID: id.String(),
+		Body:       bytes.NewReader(dataJSON),
+		Refresh:    "true",
+	}
+
+	res, err := req.Do(context.Background(), client)
+	if err != nil {
+		log.Error("Error MyLib: " + err.Error())
+		return err
+	}
+	defer res.Body.Close()
+
+	log.Warn("Logger Elasticsearch: " + l.Index)
+	return nil
+
+}
+
 func (l LoggerElastic) LoggerElasticsearch(mapping entity.MappingElastic) error {
 	log := logger.GetInstance().Logger
 
